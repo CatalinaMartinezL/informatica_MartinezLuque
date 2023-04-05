15b12d88a2866ea0ead245c3ce58602d72499630
CatalinaMartinezL <catamartinezluque@gmail.com>
2023-04-05 14:15:53 -0300

    notas de hoy

---
+++ Notas_de_clase.py
@@ -0,0 +1,12 @@
+# Para que mis cambios se hagan tambien en el repo
+# cuando termino, en bash, hago:
+# git add .
+# git commit - m "comentario"
+# git push
+
+# Para borrar una carpeta de mis archivos y de mi repo, 
+# primero lo borro de mi carpeta de archivos, despues en bash hago:
+# git rm nombre de la carpeta
+# git add .
+# git commit -m "comentario"
+# git push
